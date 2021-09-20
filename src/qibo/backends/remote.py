9096c38be8858ae50b7854f7616a1648d15c4f59
Albert Solana <iamtxena@gmail.com>
2021-09-20 09:02:52 +0200

    chore: testing new configuration

---
+++ src/qibo/backends/remote.py
@@ -0,0 +1,19 @@
+
+from qibo.backends.numpy import NumpyBackend
+from qibo.config import raise_error
+
+
+class QilimanjaroBackend(NumpyBackend):  # pragma: no cover
+    # remote backend is not tested until `qilimanjaroq` is available
+
+    description = "Uses remote devices controlled by Qilimanjaro."
+
+    def __init__(self):
+        super().__init__()
+        self.name = "qilimanjaroq"
+        import qilimanjaroq  # pylint: disable=E0401
+        from qibo import gates
+        self.is_hardware = True
+        self.hardware_module = qilimanjaroq
+        self.hardware_gates = gates
+        self.hardware_circuit = qilimanjaroq.circuit.RemoteCircuit
